a2db32ce0bc3c83e73ea22929415cb36d8caf99b
larsson91 <martin.larsson@valtech.se>
2015-02-09 14:30:05 +0100

    .

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace HelloWorld
 {
     class Program
     {
+        public static int return1()
+        {
+            return 1;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
 
 
 
 
 
+            var a = return1();
 
 
 
@@ -24,5 +30,18 @@ namespace HelloWorld
 
             Console.ReadLine();
         }
+
+        [TestFixture]
+        class Testing
+        {
+            [Test]
+            public void Test()
+            {
+                var a = return1();
+                Assert.AreEqual(1,a);
+            }
+        }
     }
+
+    
 }
