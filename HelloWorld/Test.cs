107294df601920388ed1370639cd858ece8e725d
larsson91 <martin.larsson@valtech.se>
2015-02-09 15:09:48 +0100

    .

---
+++ HelloWorld/Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HelloWorld
+{
+    [TestFixture]
+    class Test
+    {
+
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
